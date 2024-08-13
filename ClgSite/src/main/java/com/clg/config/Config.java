d9f62d5f7898b9590628ac76fca22174df61f35a
Omkar Dhole <digvijaydhole2002@example.com>
2024-08-13 17:30:39 +0530

    13-8-34

--- ClgSite/src/main/java/com/clg/config/Config.java
+++ ClgSite/src/main/java/com/clg/config/Config.java
@@ -1,6 +1,5 @@
 package com.clg.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.clg.services.Impl.SecurityCustomUserDeatilService;
 
 
