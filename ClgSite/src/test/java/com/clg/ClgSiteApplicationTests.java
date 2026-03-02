96aba1e536e2d2055e7b623d18521348f695e83f
Digvijay Dhole <digvijaydhole2002@gmail.com>
2026-03-02 21:59:56 +0530

    Errors

--- ClgSite/src/test/java/com/clg/ClgSiteApplicationTests.java
+++ ClgSite/src/test/java/com/clg/ClgSiteApplicationTests.java
@@ -2,8 +2,10 @@ package com.clg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ClgSiteApplication.class)
+@ActiveProfiles("test")
 class ClgSiteApplicationTests {
 
 	@Test
